ef03f93e4ebf990afb54f7d3be3865cb9828657a
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-21 10:56:51 +0430

    #2

--- src/main/java/af/asr/kyc/KycApplication.java
+++ src/main/java/af/asr/kyc/KycApplication.java
@@ -2,8 +2,10 @@ package af.asr.kyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(name = "jdbc.properties", value = "jdbc.properties")
 public class KycApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/af/asr/kyc/infrastructure/core/filter/ResponseCorsFilter.java
@@ -0,0 +1,36 @@
+
+package af.asr.kyc.infrastructure.core.filter;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+/**
+ * Filter that returns a response with headers that allows for Cross-Origin
+ * Requests (CORs) to be performed against the platform API.
+ */
+public class ResponseCorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(final ContainerRequest request, final ContainerResponse response) {
+
+        final ResponseBuilder resp = Response.fromResponse(response.getResponse());
+
+        resp.header("Access-Control-Allow-Origin", "*")
+        // .header("Access-Control-Expose-Headers", "Fineract-Platform-TenantId")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+        final String reqHead = request.getHeaderValue("Access-Control-Request-Headers");
+
+        if (null != reqHead && !reqHead.equals(null)) {
+            resp.header("Access-Control-Allow-Headers", reqHead);
+        }
+
+        response.setResponse(resp.build());
+
+        return response;
+    }
+}
