e59aeca72976bafadab83bd84e94b7d3a28445d9
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-21 09:36:03 +0430

    infrastructure global configurations

---
+++ src/main/java/af/asr/kyc/infrastructure/configuration/data/ExternalServicesData.java
@@ -0,0 +1,12 @@
+package af.asr.kyc.infrastructure.configuration.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ExternalServicesData {
+
+    private Long id;
+    private String name;
+}
