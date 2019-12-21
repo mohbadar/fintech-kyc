47094fd1569722a5c8e7fc926f8cc39fec3f9d7e
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-21 11:45:48 +0430

    #3

--- src/main/java/af/asr/kyc/KycApplication.java
+++ src/main/java/af/asr/kyc/KycApplication.java
@@ -2,9 +2,10 @@ package af.asr.kyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {H2ConsoleAutoConfiguration.class})
 @PropertySource(name = "jdbc.properties", value = "jdbc.properties")
 public class KycApplication {
 
