1247ddd40c9ef2806a4c54c2a001cb0fe9e06f72
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-21 09:03:11 +0430

    init stp #1

---
+++ src/main/java/af/asr/kyc/KycApplication.java
@@ -0,0 +1,13 @@
+package af.asr.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycApplication.class, args);
+	}
+
+}
