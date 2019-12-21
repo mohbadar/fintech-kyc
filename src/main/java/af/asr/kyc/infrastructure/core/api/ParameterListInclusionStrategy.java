0f8f0946c849d6d655a76548593e2bb4ab902574
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-21 10:46:28 +0430

    #2

---
+++ src/main/java/af/asr/kyc/infrastructure/core/api/ParameterListInclusionStrategy.java
@@ -0,0 +1,27 @@
+
+package af.asr.kyc.infrastructure.core.api;
+
+import java.util.Set;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+public class ParameterListInclusionStrategy implements ExclusionStrategy {
+
+    private final Set<String> parameterNamesToInclude;
+
+    public ParameterListInclusionStrategy(final Set<String> parameterNamesToSkip) {
+        this.parameterNamesToInclude = parameterNamesToSkip;
+    }
+
+    @Override
+    public boolean shouldSkipField(final FieldAttributes f) {
+        return !this.parameterNamesToInclude.contains(f.getName());
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public boolean shouldSkipClass(final Class<?> clazz) {
+        return false;
+    }
+}
